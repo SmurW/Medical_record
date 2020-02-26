779211cd7e44924bd87ce27dc8575efabf0f79fe
bulsond <bulsond@gmail.com>
2020-02-26 16:29:47 +0300

    new(SqlServerDataContext.Test): тестовый проект для классов работающих с SQL Server

---
+++ SqlServerDataContext.Tests/SqlServerDiagnosesTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlServerDataContext.Tests
+{
+    [TestClass]
+    public class SqlServerDiagnosesTests
+    {
+        [TestMethod]
+        public async Task GetDiagnosesAsync_When_Then()
+        {
+        }
+    }
+}
