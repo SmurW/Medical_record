f08e08221a01bb1598de53adeb094ab578a2d255
bulsond <bulsond@gmail.com>
2020-03-03 17:46:24 +0300

    feat(spMedications_*): Таблица Лекарств, хранимые процедуры, тестовые данные и запросы

---
+++ MedicalRecord.DataBase/dbo/StoredProcedures/spMedications_Update.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[spMedications_Update]
+	@id int = 0,
+	@name nvarchar(50),
+	@desc nvarchar(500),
+	@qp int,
+	@rp int,
+	@ap int,
+	@ru int,
+	@ad datetime,
+	@sl datetime
+AS
+BEGIN
+
+	UPDATE dbo.Medications
+	SET [Name] = @name,
+		[Description] = @desc,
+		[QuantityPackage] = @qp,
+		[RestPackages] = @rp,
+		[ArrivalPackages] = @ap,
+		[RemainedUnits] = @ru,
+		[ArrivalDate] = @ad,
+		[ShelfLife] = @sl
+	WHERE [Id] = @id;
+
+END
