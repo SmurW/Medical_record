7af418e85bac0b425ba901a6292e855f8c19b22a
bulsond <bulsond@gmail.com>
2020-03-18 20:46:11 +0300

    chore: отметки о реализованности в MsSqlDataContext

--- Medical_record/Data/MsSqlDataContext.cs
+++ Medical_record/Data/MsSqlDataContext.cs
@@ -6,16 +6,16 @@ namespace Medical_record.Data
     public class MsSqlDataContext : IDataContext
     {
         private readonly ConnectionService _connectionService;
-        public IDiagnosisDataContext Diagnoses { get; }
-        public IDoctorDataContext Doctors { get; }
-        public IExaminationDataContext Examinations { get; }
-        public IHealthGroupDataContext HealthGroups { get; }
-        public IHospitalizationDataContext Hospitalizations { get; }
-        public IMedicationsDataContext Medications { get; }
-        public IObservationDataContext Observations { get; }
-        public IPatientDataContext Patients { get; }
-        public IProcedureDataContext Procedures { get; }
-        public ISpecializationDataContext Specializations { get; }
+        public IDiagnosisDataContext Diagnoses { get; } //+
+        public IDoctorDataContext Doctors { get; } //+
+        public IExaminationDataContext Examinations { get; } //-
+        public IHealthGroupDataContext HealthGroups { get; } //+
+        public IHospitalizationDataContext Hospitalizations { get; } //-
+        public IMedicationsDataContext Medications { get; } //+
+        public IObservationDataContext Observations { get; } //-
+        public IPatientDataContext Patients { get; } //-
+        public IProcedureDataContext Procedures { get; } //+
+        public ISpecializationDataContext Specializations { get; } //+
 
         public MsSqlDataContext()
         {
